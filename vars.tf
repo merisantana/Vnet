0513fb69150d0f192f253a5b4b25f2b4cab061f6
merisantana <86296843+merisantana@users.noreply.github.com>
2021-09-23 14:02:51 +0200

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,13 @@
+Variable "resourcerm_group_name" {
+  type "string"
+}
+Variable "location" {
+  type "string"
+}
+Variable "namevnet" {
+  type "string"
+}
+
+Variable "namesqldb" {
+  type "string"
+}
